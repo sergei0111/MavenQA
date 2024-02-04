4dd9cc8dd72b1e9ad820490db407b4da136025bf
sergei <osp11011983@mail.ru>
2024-02-04 17:57:34 +0100

    cheange commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        BonusService service = new BonusService();
+        int expected = 150;
 
+        long actual = service.calculate(5000, true);
+        System.out.println(expected + " == ? == " + actual);
     }
 }
